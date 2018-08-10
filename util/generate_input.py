dd02f85cad502bc73731c8c44eb5eb2761d4d142
Felix Leupold <felixleupold90@gmail.com>
2018-08-10 15:59:37 +0200

    small fixes to generate input script

--- util/generate_input.py
+++ util/generate_input.py
@@ -3,13 +3,12 @@
 import sys
 
 def main():
-	if len(sys.argv) > 3:
+	if len(sys.argv) > 2:
 		variablesPerOrder = 1
 		if (len(sys.argv) == 4):
 			variablesPerOrder = int(sys.argv[3])
 		orders = int(sys.argv[1])
 		tokens = int(sys.argv[2])
-		print variablesPerOrder
 		# orders + bitmap + volume + price vector
 		argumentCount = (orders * variablesPerOrder) + (2 * orders) + (tokens - 1)
 		print "time ./zokrates compute-witness -a " + " ".join(["0"] * argumentCount) + " > /dev/null"
@@ -19,7 +18,7 @@ def main():
 def printUsageAndExit():
 	print "\n" \
 "Helper script to generate the arguments for the zokrates compute-witness command \n" + \
-sys.argv[0] + " <num_orders> <num_tokens> [<order encoding in number of bits>]\n" + \
+sys.argv[0] + " <num_orders> <num_tokens> [<order encoding in number of bits>]\n"
 	exit()
 
 if __name__ == '__main__':
