4c08860bbe73ab9f48ffa1c1d7bf57cb148370ce
Felix Leupold <felixleupold90@gmail.com>
2018-11-07 10:00:06 +0100

    prefix stresstest script

--- pepper/scripts/single_machine_stresstest.sh
+++ pepper/scripts/single_machine_stresstest.sh
@@ -4,11 +4,11 @@ set -e
 cd $PEPPER/pepper
 
 function run {
-	./pepper_compile_and_setup_P.sh hash_transform
-	/usr/bin/time -v ./pepper_compile_and_setup_V.sh hash_transform transform.vkey transform.pkey
+	./pepper_compile_and_setup_P.sh hash_transform | sed "s/^/[$COUNT Setup Prover] /"
+	/usr/bin/time -v ./pepper_compile_and_setup_V.sh hash_transform transform.vkey transform.pkey | sed "s/^/[$COUNT Setup Verifier] /"
 
-	/usr/bin/time -v ./bin/pepper_prover_hash_transform prove transform.pkey transform.inputs transform.outputs transform.proof
-	/usr/bin/time -v ./bin/pepper_verifier_hash_transform verify transform.vkey transform.inputs transform.outputs transform.proof
+	/usr/bin/time -v ./bin/pepper_prover_hash_transform prove transform.pkey transform.inputs transform.outputs transform.proof | sed "s/^/[$COUNT Prove] /"
+	/usr/bin/time -v ./bin/pepper_verifier_hash_transform verify transform.vkey transform.inputs transform.outputs transform.proof | sed "s/^/[$COUNT Verify] /"
 }
 
 # remove assert and provide input
@@ -16,21 +16,25 @@ sed -i 's%assert_zero(result - input->shaHash);%//assert_zero(result - input->sh
 echo 62174266130574243119709474530137211798696360945565673697823515822613061023144 > prover_verifier_shared/transform.inputs
 
 # 50M constraints
+COUNT='50M'
 echo '50 million constraints'
 sed -i 's/#define ORDERS [0-9]*/#define ORDERS 725/g' apps/hash_transform.h
 run
 
 # 100M constraints
+COUNT='100M'
 echo '100 million constraints'
 sed -i 's/#define ORDERS [0-9]*/#define ORDERS 1450/g' apps/hash_transform.h
 run
 
 # 150M constraints
+COUNT='150M'
 echo '150 million constraints'
 sed -i 's/#define ORDERS [0-9]*/#define ORDERS 2175/g' apps/hash_transform.h
 run
 
 # 200M constraints
+COUNT='200M'
 echo '200 million constraints'
 sed -i 's/#define ORDERS [0-9]*/#define ORDERS 2900/g' apps/hash_transform.h
 run
