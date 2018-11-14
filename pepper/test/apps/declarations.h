168604dc5a8406c7410662fddceb4278ca3ed198
josojo <josojo@hotmail.de>
2018-11-14 16:23:25 +0100

    more generalized test declarations

---
+++ pepper/test/apps/declarations.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+//Declaration of internal pepper methods (to make compiler happy)
+void exo_compute(uint32_t**, uint32_t*, void*, uint32_t);
+void ext_gadget(void*, void*, uint32_t);
+void assert_zero(uint32_t);
+typedef long int128;
