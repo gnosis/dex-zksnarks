b32225aabd99f1d8a7a15715b550e565c0f684e6
Felix Leupold <felixleupold90@gmail.com>
2018-12-06 16:32:07 +0000

    adding a script that can convert libsnark binary output to non binary

---
+++ pepper/scripts/pepper_binary_format_reader.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <libsnark/common/default_types/r1cs_ppzksnark_pp.hpp>
+
+using namespace std;
+
+typedef libff::Fr<libsnark::default_r1cs_ppzksnark_pp> FieldT;
+
+int main(int argc, char **argv) 
+{   
+	if (argc != 3) {
+		std::cout << "Usage: " << argv[0] << " <input> <output>\n";
+		return 1;
+	}
+    libsnark::default_r1cs_ppzksnark_pp::init_public_params();
+
+	ifstream source(argv[1], std::ios::binary);
+    FILE *target = fopen(argv[2], "w");
+    FieldT current;
+    mpz_t r; mpz_init(r);
+
+    // Field elements are seperated by space. 
+    // Read a single element, then get one character (space) until EOF
+    do {
+        source >> current;
+        current.as_bigint().to_mpz(r);
+        gmp_fprintf(target, "%Zd ", r);
+    } while (source.get() > 0);
+
+    source.close();
+    fclose(target);
+}
