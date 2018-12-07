28dae975985435982a6ede5557a6ce27e89c6c29
Felix Leupold <felixleupold90@gmail.com>
2018-12-07 16:16:09 +0000

    fix indentation

--- pepper/ext_gadget/pedersen_bridge.cpp
+++ pepper/ext_gadget/pedersen_bridge.cpp
@@ -25,7 +25,7 @@ protoboard<FieldT> getProtoboard(const char* assignment)
     const ethsnarks::jubjub::Params params;
     protoboard<FieldT> pb;
 
-	ethsnarks::VariableArrayT in;
+    ethsnarks::VariableArrayT in;
     // The gadget expects input that is a multiple of three. Thus, we pad with 0s.
     size_t padding  = (3 - (inputSize() % 3)) % 3;
     size_t scalar_size = inputSize() + padding;
@@ -34,8 +34,8 @@ protoboard<FieldT> getProtoboard(const char* assignment)
     ethsnarks::jubjub::PedersenHash f(pb, params, "dex.pedersen-hash", in, "gadget");
     f.generate_r1cs_constraints();
 
-	if (assignment) {
-		libff::bit_vector in_bv;
+    if (assignment) {
+	libff::bit_vector in_bv;
      	for (int i = 0; i < scalar_size; i++) {
             if (i < inputSize()) {
                 in_bv.push_back(assignment[2*i] - '0');
@@ -44,10 +44,10 @@ protoboard<FieldT> getProtoboard(const char* assignment)
             }
     	}
 
-		in.fill_with_bits(pb, in_bv);
+	in.fill_with_bits(pb, in_bv);
      	f.generate_r1cs_witness();
-		assert(pb.is_satisfied());
-	}
+	assert(pb.is_satisfied());
+    }
     
     return pb;
 }
