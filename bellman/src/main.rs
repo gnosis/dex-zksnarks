9ffbed1132a8cc9170850dff5e398216f637bf8a
Felix Leupold <felixleupold90@gmail.com>
2018-12-10 21:41:42 +0100

    allowing to read from command line + usage

--- bellman/src/main.rs
+++ bellman/src/main.rs
@@ -22,14 +22,22 @@ struct ParsedLine<E: Engine> {
     result: E::Fr,
 }
 
-struct CircuitImpl {
-    witness: String,
-    qap_a: String,
-    qap_b: String,
-    qap_c: String,
+struct Assignment<E: Engine> {
+    variable: Variable,
+    constrained: bool,
+    value: E::Fr,
 }
 
-fn parse_line<E: Engine>(line: &String, variables: &mut Vec<(Variable, bool, E::Fr)>, negate: bool) -> ParsedLine<E> {
+fn empty_line<E:Engine>(index: usize) -> ParsedLine<E> {
+    ParsedLine::<E>{
+        index:index, 
+        lc: LinearCombination::zero(), 
+        debug: "".to_string(), 
+        result: E::Fr::zero(),
+    }
+}
+
+fn parse_line<E: Engine>(line: &String, variables: &mut Vec<Assignment<E>>, negate: bool) -> ParsedLine<E> {
     let parts: Vec<&str> = line.split_whitespace().collect();
     let row: usize = parts[0].parse().unwrap();
     let column: usize = parts[1].parse().unwrap();
@@ -42,18 +50,22 @@ fn parse_line<E: Engine>(line: &String, variables: &mut Vec<(Variable, bool, E::
     if parts[2].starts_with("-") { 
         coefficient.negate();
     }
-    variables[row] = (variables[row].0, true, variables[row].2);
+    variables[row] = Assignment::<E>{
+        variable: variables[row].variable, 
+        constrained: true, 
+        value: variables[row].value
+    };
     let mut result = coefficient;
-    result.mul_assign(&(variables[row].2));
+    result.mul_assign(&(variables[row].value));
     ParsedLine::<E> {
         index: column,
-        lc: LinearCombination::zero() + (coefficient, variables[row].0),
-        debug: format!("({}*{:?})", coefficient, variables[row].2),
+        lc: LinearCombination::zero() + (coefficient, variables[row].variable),
+        debug: format!("({}*{:?})", coefficient, variables[row].value),
         result: result,
     }
 }
 
-fn read_next_constraint<E: Engine>(file: &mut BufReader<File>, variables: &mut Vec<(Variable, bool, E::Fr)>, negate: bool) -> Option<ParsedLine<E>> {
+fn read_next_constraint<E: Engine>(file: &mut BufReader<File>, variables: &mut Vec<Assignment<E>>, negate: bool) -> Option<ParsedLine<E>> {
     let mut line = String::new();
     let len = file.read_line(&mut line).unwrap();
     if len > 0 {
@@ -63,52 +75,76 @@ fn read_next_constraint<E: Engine>(file: &mut BufReader<File>, variables: &mut V
     }
 }
 
-// Implementation of our circuit.
-impl<'a, E: Engine> Circuit<E> for &'a CircuitImpl {
-    fn synthesize<CS: ConstraintSystem<E>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
-        
-        let now = Instant::now();
-        println!("  {} Reading Witness", now.elapsed().as_secs());
-        let mut vars: Vec<(Variable, bool /* constrained */, E::Fr)> = Vec::new();
-        vars.push((CS::one(), false, E::Fr::one()));
-        {
-            let file = File::open(&self.witness)?;
-            for line in BufReader::new(file).lines() {
-                for (i, var) in line.expect("Couldn't parse line").split_whitespace().enumerate() {
-                    let value =  E::Fr::from_str(var).unwrap();
-                    let var = cs.alloc(|| var, || Ok(value))?;
-                    vars.push((var, false, value));
-                }
-            }
+fn parse_witness<F: PrimeField>(filename: &String) -> Vec<F> {
+    let file = File::open(&filename).unwrap();
+    let mut result = Vec::new();
+    for line in BufReader::new(file).lines() {
+        for var in line.expect("Couldn't parse line").split_whitespace() {
+            result.push(F::from_str(var).unwrap());
         }
+    }
+    result
+}
+
+fn add_parsed_lines<E: Engine>(mut lhs: ParsedLine<E>,rhs: &ParsedLine<E>) -> ParsedLine<E> {
+    assert!(lhs.index == rhs.index);
+    lhs.result.add_assign(&rhs.result);
+    ParsedLine {
+        index: lhs.index,
+        lc: lhs.lc + &rhs.lc,
+        debug: lhs.debug + " + " + &rhs.debug,
+        result: lhs.result,
+    }
+}
+
+struct CircuitImpl<F: PrimeField> {
+    public_witness: Vec<F>,
+    aux_witness: Vec<F>,
+    qap_a: String,
+    qap_b: String,
+    qap_c: String,
+}
+
+impl<'a, E: Engine> Circuit<E> for &'a CircuitImpl<E::Fr> {
+    fn synthesize<CS: ConstraintSystem<E>>(self, cs: &mut CS) -> Result<(), SynthesisError> {
+        let now = Instant::now();
+
+        println!("  {} Reading Witness", now.elapsed().as_secs());
+        let aux: Vec<Assignment<E>> = self.aux_witness.clone().into_iter()
+            .map(|value| Assignment {
+                variable: cs.alloc(|| "aux", || Ok(value)).unwrap(), 
+                constrained: false, 
+                value: value
+            }).collect();
+        let public: Vec<Assignment<E>> = self.public_witness.clone().into_iter()
+            .map(|value| Assignment {
+                variable: cs.alloc_input(|| "aux", || Ok(value)).unwrap(), 
+                constrained: false, 
+                value: value
+            }).collect();
+
+        let mut vars = vec![
+            Assignment::<E>{
+                variable: CS::one(), 
+                constrained: false, 
+                value: E::Fr::one()
+            }];
+        vars.extend(aux);
+        vars.extend(public);
+
         println!("  {} Reading QAP", now.elapsed().as_secs());
         {
             let mut qap_a_file = BufReader::new(File::open(&self.qap_a)?);
             let mut qap_b_file = BufReader::new(File::open(&self.qap_b)?);
             let mut qap_c_file = BufReader::new(File::open(&self.qap_c)?);
 
-            let mut current_a_lc = ParsedLine::<E>{
-                index:0 , 
-                lc: LinearCombination::zero(), 
-                debug: "".to_string(),
-                result: E::Fr::zero(),
-            };
+            let mut current_a_lc = empty_line(0);
             let mut next_a_lc = read_next_constraint::<E>(&mut qap_a_file, &mut vars, false);
 
-            let mut current_b_lc = ParsedLine::<E>{
-                index:0,
-                lc: LinearCombination::zero(),
-                debug: "".to_string(),
-                result: E::Fr::zero()
-            };
+            let mut current_b_lc = empty_line(0);
             let mut next_b_lc = read_next_constraint::<E>(&mut qap_b_file, &mut vars, false);
 
-            let mut current_c_lc = ParsedLine::<E>{
-                index:0 , 
-                lc: LinearCombination::zero(), 
-                debug: "".to_string(), 
-                result: E::Fr::zero(),
-            };
+            let mut current_c_lc = empty_line(0);
             let mut next_c_lc = read_next_constraint::<E>(&mut qap_c_file, &mut vars, true);
 
             while next_a_lc.is_some() || next_b_lc.is_some() || next_c_lc.is_some() {
@@ -118,9 +154,7 @@ impl<'a, E: Engine> Circuit<E> for &'a CircuitImpl {
                         let next = next_a_lc.as_ref().unwrap();
                         assert!(next.index >= current_a_lc.index);
                         if next.index == current_a_lc.index {
-                            current_a_lc.lc = current_a_lc.lc + &next.lc;
-                            current_a_lc.debug = current_a_lc.debug + " + " + &next.debug;
-                            current_a_lc.result.add_assign(&next.result);
+                            current_a_lc = add_parsed_lines::<E>(current_a_lc, next);
                             advance = true;
                         }
                     }
@@ -184,41 +218,26 @@ impl<'a, E: Engine> Circuit<E> for &'a CircuitImpl {
                 assert!(current_a_lc.result == current_c_lc.result, 
                 format!("constraint {} failed: {} * {} != {}", current_a_lc.index, current_a_lc.debug, current_b_lc.debug, current_c_lc.debug));
 
-                current_a_lc = ParsedLine::<E>{
-                    index:current_a_lc.index+1 , 
-                    lc: LinearCombination::zero(), 
-                    debug: "".to_string(), 
-                    result: E::Fr::zero(),
-                };
-                current_b_lc = ParsedLine::<E>{
-                    index:current_b_lc.index+1 ,
-                    lc: LinearCombination::zero(),
-                    debug: "".to_string(),
-                    result: E::Fr::zero(),
-                };
-                current_c_lc = ParsedLine::<E>{
-                    index:current_c_lc.index+1 ,
-                    lc: LinearCombination::zero(),
-                    debug: "".to_string(),
-                    result: E::Fr::zero(),
-                };
+                current_a_lc = empty_line(current_a_lc.index + 1);
+                current_b_lc = empty_line(current_b_lc.index + 1);
+                current_c_lc = empty_line(current_c_lc.index + 1);
             }
         }
 
-        /**
+        /*
          * For some reason, pepper can include variables in the witness that are not constrained.
          * Bellmann asserts that this is not the case. We could clean up the witness from unbound
          * variables and shift all constraints appropriately but adding dummy constraints seems to
          * be the easier option for now.
          */
-        for (i, item) in vars.iter().enumerate() {
-            if !item.1 {
-                println!("[Warning] Unconstrained variable {:?}", item.0);
+        for var in vars.iter() {
+            if !var.constrained {
+                println!("[Warning] Unconstrained variable {:?}", var.variable);
                 cs.enforce(
-                    || format!("unused var {}", i),
-                    |lc| lc + item.0,
+                    || format!("unused var {:?}", var.variable),
+                    |lc| lc + var.variable,
                     |lc| lc + CS::one(),
-                    |lc| lc + item.0,
+                    |lc| lc + var.variable,
                 );
             }
         }
@@ -228,8 +247,31 @@ impl<'a, E: Engine> Circuit<E> for &'a CircuitImpl {
     }
 }
 
-// Create some parameters, create a proof, and verify the proof.
+fn print_usage(args: Vec<String>) {
+    println!("
+Program to create a zkSnark proof for an R1CS generated by the pepper framework.
+It requires the R1CS (A, B, C matrices) as well as assignments for public and auxiliary input.
+
+The witnesses are consumed from a comma separated list of field elements.
+The matrices are consumed as files where each line corresponds to a nonzero entry in the corresponding QAP matrix. 
+E.g. a line `96 5 2` means the entry at 96th row, 5th column is 2.
+
+The index (row) of a public input i_n is |aux| + n. The entire witness is prepended with 1 in 0th position.
+
+By applying the witness, the matrices are expected to satisfy (A * B) + C = 0
+
+Usage:
+    {} <public witness> <aux witness> <A matrix> <B matrix> <C matrix>", args[0])
+}
+
 fn main() {
+    use std::env;
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 6 {
+        print_usage(args);
+        ::std::process::exit(1);
+    }
+
     use pairing::bn256::{Bn256, Fr};
     use rand::thread_rng;
     use std::marker::PhantomData;
@@ -239,11 +281,15 @@ fn main() {
     };
 
     let rng = &mut thread_rng();
+
+    let public = parse_witness(&args[1]);
+    let aux = parse_witness(&args[2]);
     let c = CircuitImpl {
-        witness: "data/hash_transform.witness".to_string(),
-        qap_a: "data/hash_transform.qap.matrix_a".to_string(),
-        qap_b: "data/hash_transform.qap.matrix_b".to_string(),
-        qap_c: "data/hash_transform.qap.matrix_c".to_string()
+        public_witness: public.clone(),
+        aux_witness: aux,
+        qap_a: args[3].clone(),
+        qap_b: args[4].clone(),
+        qap_c: args[5].clone(),
     };
 
     let now = Instant::now();
@@ -259,7 +305,9 @@ fn main() {
     let proof = create_random_proof(&c, &params, rng).unwrap();
 
     println!("{} start verify_proof", now.elapsed().as_secs());
-    match verify_proof(&pvk, &proof, &[]) {
+
+    let file = File::open(&args[1]).unwrap();
+    match verify_proof(&pvk, &proof, &public) {
         Ok(v) => assert!(v),
         Err(e) => println!("error: {:?}", e)
     }
