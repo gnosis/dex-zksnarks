35e975a9437c00b3bdf5225642ce42f82c042133
Felix Leupold <felixleupold90@gmail.com>
2018-12-11 14:16:37 +0100

    Making PH Gadget return result

--- pepper/ext_gadget/pedersen_bridge.cpp
+++ pepper/ext_gadget/pedersen_bridge.cpp
@@ -1,14 +1,15 @@
 #include "common.h"
 #include "jubjub/pedersen_hash.hpp"
 #include "jubjub/params.hpp"
+#include "utils.hpp"
 
 using namespace libsnark;
 using namespace libff;
 
 #define BUFLEN 10240
 
-#define LEFT_INPUT_SIZE 254
-#define RIGHT_INPUT_SIZE 254
+#define LEFT_INPUT_SIZE 255
+#define RIGHT_INPUT_SIZE 255
 #define OUTPUT_SIZE 2
 
 uint64_t inputSize() {
@@ -27,26 +28,32 @@ protoboard<FieldT> getProtoboard(const char* assignment)
 
     ethsnarks::VariableArrayT in;
     // The gadget expects input that is a multiple of three. Thus, we pad with 0s.
-    size_t padding  = (3 - (inputSize() % 3)) % 3;
-    size_t scalar_size = inputSize() + padding;
-    in.allocate(pb, scalar_size, FMT("annotation_prefix", " scaler to multiply by"));
+    assert(inputSize() % 3 == 0);
+    in.allocate(pb, inputSize(), FMT("bridge", " scaler to multiply by"));
+
+    ethsnarks::VariableT result_x = ethsnarks::make_variable(pb, FMT("bridge", "result_x"));
+    ethsnarks::VariableT result_y = ethsnarks::make_variable(pb, FMT("bridge", "result_y"));
 
     ethsnarks::jubjub::PedersenHash f(pb, params, "dex.pedersen-hash", in, "gadget");
     f.generate_r1cs_constraints();
 
-    if (assignment) {
-	libff::bit_vector in_bv;
-     	for (int i = 0; i < scalar_size; i++) {
-            if (i < inputSize()) {
-                in_bv.push_back(assignment[2*i] - '0');
-            } else {
-                in_bv.push_back(false);
-            }
-    	}
+    pb.add_r1cs_constraint(
+        ethsnarks::ConstraintT(result_x, 1, f.result_x()),
+            FMT("bridge", "bridge.result_x=gadget.result_x"));
+    pb.add_r1cs_constraint(
+        ethsnarks::ConstraintT(result_y, 1, f.result_y()),
+            FMT("bridge", "bridge.result_y=gadget.result_y"));
 
-	in.fill_with_bits(pb, in_bv);
-     	f.generate_r1cs_witness();
-	assert(pb.is_satisfied());
+    if (assignment) {
+        libff::bit_vector in_bv;
+        for (int i = 0; i < inputSize(); i++) {
+            in_bv.push_back(assignment[2*i] - '0');
+        }
+        in.fill_with_bits(pb, in_bv);
+        f.generate_r1cs_witness();
+        pb.val(result_x) = pb.val(f.result_x());
+        pb.val(result_y) = pb.val(f.result_y());
+        assert(pb.is_satisfied());
     }
     
     return pb;
--- pepper/test/e2e/hash_transform.sh
+++ pepper/test/e2e/hash_transform.sh
@@ -14,3 +14,5 @@ echo 032701115855630478187979287852631045544 >> prover_verifier_shared/transform
 
 ./bin/pepper_prover_hash_transform prove transform.pkey transform.inputs transform.outputs transform.proof
 ./bin/pepper_verifier_hash_transform verify transform.vkey transform.inputs transform.outputs transform.proof
+
+diff ./prover_verifier_shared/transform.outputs <(printf "0\n2709151994604095268253795983695773831904488672374264037853826788765176428835\n")
